bc9193810d36ac386c3ef53ee5e1efc5a05de62b
Jakob Fischer <jakobfischer93@gmail.com>
2019-11-06 21:21:01 +0100

    Removed tls stuff

--- src/persistence/impl_pg/connection.rs
+++ src/persistence/impl_pg/connection.rs
@@ -1,5 +1,4 @@
 use postgres::{Connection, TlsMode};
-use postgres::tls;
 use std::{thread, time};
 
 use crate::persistence::DaoError;
@@ -7,10 +6,9 @@ use crate::persistence::DaoError;
 pub fn try_connect(pg_params: &str) -> Result<Connection, DaoError> {
     const MAX_TRIES: u64 = 5;
 	const SLEEP_FACTOR: u64 = 1000;
-    let negotiator = NativeTls::new()?;
 	debug!("Connecting to db with '{}'", pg_params);
     for i in 0..MAX_TRIES + 1 {
-        match Connection::connect(pg_params, TlsMode::Require(&negotiator)) {
+        match Connection::connect(pg_params, TlsMode::None) {
             Ok(c) => return Ok(c),
             Err(e) => {
 				if i == MAX_TRIES {
