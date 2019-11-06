8b12519f8531479fd8d22b99ad1a250c76bd127c
Jakob Fischer <jakobfischer93@gmail.com>
2019-11-06 21:04:36 +0100

    Started working on tls to db

--- src/persistence/impl_pg/connection.rs
+++ src/persistence/impl_pg/connection.rs
@@ -1,4 +1,5 @@
 use postgres::{Connection, TlsMode};
+use postgres::tls;
 use std::{thread, time};
 
 use crate::persistence::DaoError;
@@ -6,9 +7,10 @@ use crate::persistence::DaoError;
 pub fn try_connect(pg_params: &str) -> Result<Connection, DaoError> {
     const MAX_TRIES: u64 = 5;
 	const SLEEP_FACTOR: u64 = 1000;
+    let negotiator = NativeTls::new()?;
 	debug!("Connecting to db with '{}'", pg_params);
     for i in 0..MAX_TRIES + 1 {
-        match Connection::connect(pg_params, TlsMode::None) {
+        match Connection::connect(pg_params, TlsMode::Require(&negotiator)) {
             Ok(c) => return Ok(c),
             Err(e) => {
 				if i == MAX_TRIES {
